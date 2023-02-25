a37bce87d8a4f60f61f8bfe98621fd095a7acee3
Sarai6e <1472173@senati.pe>
2023-02-24 22:28:27 -0500

    Desarrollar algoritmos

---
+++ lista de numeros divisibles de 5 .py
@@ -0,0 +1,6 @@
+#
+num=[10,20,33,46,55]
+print('los numeros divisibles de 5 son:')
+for i in range (0,5):
+    if(num[i]%5==0):
+        print(num[i]) 
---
+++ suma actual y anterior.py
@@ -0,0 +1,4 @@
+# Desarrollar un algoritmo en la que se muestre la suma actual y su anterior (10):     
+for i in range(0,11):  #Crear bucle
+    print("La suma es: ")    #print (función)
+    print(i,"+",i-1,"=",i+(i-1))
