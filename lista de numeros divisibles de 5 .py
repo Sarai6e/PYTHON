6a927b9b2748c7a459dbdfcae438bf116036482a
Sarai6e <125670731+Sarai6e@users.noreply.github.com>
2023-02-24 23:02:23 -0500

    Update lista de numeros divisibles de 5 .py

--- lista de numeros divisibles de 5 .py
+++ lista de numeros divisibles de 5 .py
@@ -1,6 +1,6 @@
-#
-num=[10,20,33,46,55]
+## Desarrolle un algoritmo que muestre los números divisibles entre 5 de una lista de números:
+num=[10,20,33,46,55]  #Función para una operación
 print('los numeros divisibles de 5 son:')
-for i in range (0,5):
-    if(num[i]%5==0):
+for i in range (0,5):  #bucle
+    if(num[i]%5==0):    #Funcion para divisor de 5
         print(num[i]) 
