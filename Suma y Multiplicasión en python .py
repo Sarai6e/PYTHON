632679fe677ad3718ff60b6d61964e7fa0dc4aa0
Sarai6e <1472173@senati.pe>
2023-02-25 10:01:06 -0500

    mostrar patrones

---
+++ Suma y Multiplicasión en python .py
@@ -0,0 +1,13 @@
+# Desarrollar algoritmos de suma y multiplicación que contengan 
+# dos variables.
+
+# Suma(operador)
+a=34              # x,y= Variables
+b=12
+suma=a+b   #Funcion para una operacion
+print("La suma es:",a+b)  #Print: Es una funcion
+
+# Multiplicasión(operador)
+multiplicasion=a*b  #Funcion para una operacion
+print("La multiplicasión es:",a*b) #Print: Es una funcion
+
