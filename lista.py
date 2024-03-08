f25bbe23916a52a1fd86753ded4940e2efd2e7b9
Sarai6e <1472173@senati.pe>
2024-03-08 12:46:19 -0500

    lista y patrones

---
+++ lista.py
@@ -0,0 +1,7 @@
+# Desarrolle un algoitmo que escriba un codigo de pogramacin donde me permita imprimir el primer y el ultimo valor de lalista.
+list=['red ',' blue',' green',' black',' white']   #List ordena los datos 
+n= list[0]                               
+m=list[-1]
+
+print(n)   #Imprime la funcion del primer valor de la lista
+print(m)   #Imprime la funcion del ultimo valor de la lista
